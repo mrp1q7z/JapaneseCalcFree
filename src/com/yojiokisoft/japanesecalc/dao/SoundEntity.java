811fd7a6ee009f714ec149cce87bd0636f7bc61c
mrp1q7z <mrp1q7z@gmail.com>
2013-08-28 15:19:28 +0900

    新環境(Kepler)で動くようにした

--- src/com/yojiokisoft/japanesecalc/dao/SoundEntity.java
+++ src/com/yojiokisoft/japanesecalc/dao/SoundEntity.java
@@ -21,6 +21,8 @@ import java.io.Serializable;
  * サウンドエンティティ
  */
 public class SoundEntity implements Serializable {
+	private static final long serialVersionUID = 1775444377602465547L;
+
 	/** リソースID */
 	public int resId;
 
